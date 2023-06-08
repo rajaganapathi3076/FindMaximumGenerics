a23cf9aacab9999435946d0d38c51e5d27261683
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-06-08 16:54:46 +0530

    adding Uc1

---
+++ FindMaximumGenerics/MaximumNumberFinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumGenerics
+{
+    internal class MaximumNumberFinder
+    {
+        public static int FindMaximumNumber(int num1, int num2, int num3)
+        {
+            int maxNumber = num1;
+
+            if (num2.CompareTo(maxNumber) > 0)
+            {
+                maxNumber = num2;
+            }
+
+            if (num3.CompareTo(maxNumber) > 0)
+            {
+                maxNumber = num3;
+            }
+
+            return maxNumber;
+        }
+    }
+}
---
+++ FindMaximumGenerics/Program.cs
@@ -0,0 +1,22 @@
+﻿using FindMaximumGenerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to FindMaximumGenericsUC1");
+        int max1 = MaximumNumberFinder.FindMaximumNumber(10, 5, 7);
+        Console.WriteLine("Max Number: " + max1); // Output: Max Number: 10
+
+        // Test Case 2: Max Number at 2nd position
+        int max2 = MaximumNumberFinder.FindMaximumNumber(4, 8, 3);
+        Console.WriteLine("Max Number: " + max2); // Output: Max Number: 8
+
+        // Test Case 3: Max Number at 3rd position
+        int max3 = MaximumNumberFinder.FindMaximumNumber(2, 6, 9);
+        Console.WriteLine("Max Number: " + max3); // Output: Max Number: 9
+    }
+}
+
+    
+
